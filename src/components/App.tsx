e013f16098517fac967f7c3898d49d7638946b8b
Puncoz Nepal <puncoz138@gmail.com>
2019-10-27 12:07:42 +0545

    test and build

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,16 @@
 import React, { FunctionComponent } from "react"
+import { Provider } from "react-redux"
+import configureStores from "../stores"
 import { Header } from "./header"
 
+const store = configureStores()
+
 const App: FunctionComponent = () => (
-    <div className="container">
-        <Header />
-    </div>
+    <Provider store={store}>
+        <div className="container">
+            <Header />
+        </div>
+    </Provider>
 )
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,10 @@
 import React from "react"
 import { render } from "react-dom"
-import { Provider } from "react-redux"
 import "./assets/sass/index.scss"
 import App from "./components/App"
 import * as serviceWorker from "./serviceWorker"
-import configureStores from "./stores"
 
-const store = configureStores()
-
-const Root = () => (
-    <Provider store={store}>
-        <App />
-    </Provider>
-)
-
-render(<Root />, document.getElementById("root"))
+render(<App />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
